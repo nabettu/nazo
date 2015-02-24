6d5cba2b28026b70c9ee5a8acb80979672074e40
tatsuakiw <tatsuaki.w@gmail.com>
2015-02-24 10:31:32 +0900

    modalの中身が問題によって変わる

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 var sheetId ="1_-Of24WGgGGq1SfdjqaglwN9OfMweMVbS7pMtgCmKh4";//なぞなぞデータの入ったエクセル
 var xlsJson;//エクセルのデータのJson
 
-//Spredsheetのjsonを持ってくる
+//Spredsheetのjsonを持ってくるURL
 var requestUrl = "https://spreadsheets.google.com/feeds/list/"+sheetId+"/od6/public/values?alt=json";
-$.get(requestUrl,
-  {dataType:"json"},
-  function(data){
-    //リクエストが成功した際に実行する関数
-    //console.log(data);
-    xlsJson = data.feed.entry;
-    //jsonの結果をHTML出力する関数へ入力する
-    Json2html(xlsJson);
-  }
-);
+
+window.onload = function(){
+  xlsLoad(requestUrl);
+};
+
+function xlsLoad(requestUrl){
+  $.get(requestUrl,
+    {dataType:"json"},
+    function(data){
+      //リクエストが成功した際に実行する関数
+      //console.log(data);
+      xlsJson = data.feed.entry;
+      //jsonの結果をHTML出力する関数へ入力する
+      Json2html(xlsJson);
+    }
+  );
+}
 
 function Json2html(data){
   var html="";
@@ -20,7 +27,7 @@ function Json2html(data){
     for(var j = 0;j<5;j++){
       var n = i*5+j;
       html+="<div class='hintList'>";
-      html+="<a href='#hintModal' data-toggle='modal' onclick='hintModalContent("+(n+1)+");''>";
+      html+="<a href='#hintModal' data-toggle='modal' onclick='hintModalContent("+n+");''>";
       html+="<img src='"+data[n].gsx$image.$t+"' class='listImg'/>";
       html+="<div class='hintTitle'>第"+(n+1)+"問</div>";
       html+="<div class='hintContents'>"+data[n].gsx$comment.$t+"</div>";
@@ -30,4 +37,14 @@ function Json2html(data){
     html="";
   }
 //  console.log($("#siteAbout"));
+}
+
+function hintModalContent(number){
+  $("#hintNumber")[0].innerHTML = "第"+(number+1)+"問";
+  $("#hintModalQ")[0].innerHTML = xlsJson[number].gsx$question.$t;
+  $("#hintModalImg")[0].src = xlsJson[number].gsx$image.$t;
+  $("#hint")[0].innerHTML = xlsJson[number].gsx$hint.$t;
+  $("#answer")[0].innerHTML = "答え："+ xlsJson[number].gsx$answer.$t;
+  $("#explanation")[0].innerHTML = xlsJson[number].gsx$explanation.$t;
+  //console.log(xlsJson);
 }
